13dcaef8f3bfcd7e8419a2421cea2ad91adb6eaf
jsrobinson2311 <48560387+jsrobinson2311@users.noreply.github.com>
2019-03-14 14:58:50 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hGDiE-1zR9AH1Jvs94LXa7g3cckhdBFyoXTgSQ_TtCE/edit#gid=0';
